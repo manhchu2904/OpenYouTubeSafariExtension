4a8cf51ef89efa7db0b20b3a8f395d4e8fc0ecd7
manhchu2904 <chuducmanh2904@gmail.com>
2026-07-06 14:54:05 +0000

    Hello

--- OpenYouTubeSafariExtension.appex/content.js
+++ OpenYouTubeSafariExtension.appex/content.js
@@ -1,33 +1,35 @@
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
+  console.log("Received response: ", response);
 });
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+  if (request.action === "OYT")
+    window.location.href = window.location.href.replace(
+      window.location.protocol,
+      "youtube:",
+    );
+
+  console.log("Received request: ", request);
 });
 
 function afterNavigate() {
-    var locationArr = window.location.pathname.split("/").reverse()
-    if (locationArr.includes("watch")) {
-        window.location.href = `youtube://watch${window.location.search}`
-    } else if (locationArr.includes("live")) {
-        window.location.href = `youtube://live/${locationArr[0]}`
-    } else if (locationArr.includes("shorts")) {
-        window.location.href = `youtube://shorts/${locationArr[0]}`
-    } else if (locationArr.includes("clip")) {
-        window.location.href = `youtube://clip/${locationArr[0]}`
-    } else if (locationArr.includes("channel")) {
-        window.location.href = `youtube://channel/${locationArr[0]}`
-    } else if (locationArr[0] && !locationArr[1]) {
-        window.location.href = `youtube://${locationArr[0]}`
-    }
+  const { pathname, href, protocol } = window.location;
+  const routes = ["/watch", "/live", "/shorts", "/clip", "/channel"];
+  if (
+    routes.some((route) => pathname.startsWith(route)) ||
+    /^\/@[^/]+/.test(pathname)
+  ) {
+    window.location.href = href.replace(protocol, "youtube:");
+  }
 }
 
-(document.body || document.documentElement).addEventListener('transitionend',
-  function(/*TransitionEvent*/ event) {
-    if (event.propertyName === 'width' && event.target.id === 'progress') {
-        afterNavigate();
+(document.body || document.documentElement).addEventListener(
+  "transitionend",
+  function (event) {
+    if (event.propertyName === "width" && event.target.id === "progress") {
+      afterNavigate();
     }
-}, true);
-// After page load
+  },
+  true,
+);
 afterNavigate();
